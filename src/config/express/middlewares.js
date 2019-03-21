3bb1e0561ea026fc8deb8ddf9f5a5e96c21dbcf7
Marco Lau <mlau@palo-it.com>
2019-03-21 17:58:58 +0800

    Add helmet

--- src/config/express/middlewares.js
+++ src/config/express/middlewares.js
@@ -1,11 +1,13 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, `../../../config.env.${process.env.NODE_ENV}`) });
 
 export const middlewares = (app) => {
   app.use(bodyParser.json());
+  app.use(helmet());
   return app;
 };
 
