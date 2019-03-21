8a1b367d0028719f2020b53db1f1291587655e07
Marco Lau <mlau@palo-it.com>
2019-03-21 16:15:29 +0800

    Rename to NODE_ENV

--- src/config/express/middlewares.js
+++ src/config/express/middlewares.js
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, `../../../config.env.${process.env.ENVIRONMENT}`) });
+dotenv.config({ path: path.resolve(__dirname, `../../../config.env.${process.env.NODE_ENV}`) });
 
 export const middlewares = (app) => {
   app.use(bodyParser.json());
