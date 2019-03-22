3b99172478071da6a85174d4cfe6b3f62a4ee655
Marco Lau <mlau@palo-it.com>
2019-03-22 12:11:35 +0800

    Add cors middleware

--- src/config/express/middlewares.js
+++ src/config/express/middlewares.js
@@ -1,8 +1,14 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import helmet from 'helmet';
+
+const corsOptions = {
+  optionsSuccessStatus: 200,
+}
 
 const middlewares = (app) => {
   app.use(bodyParser.json());
+  app.use(cors(corsOptions));
   app.use(helmet());
   return app;
 };
